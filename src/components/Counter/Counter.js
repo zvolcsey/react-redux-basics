fb0bcfa1382fb17578dbaef64e2d9a2ca80cac24
Zoltán Völcsey <zvolcsey@gmail.com>
2022-03-02 17:12:37 +0100

    practice to working with multiple state properties

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -7,35 +7,66 @@ import Button from '../UI/Button';
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
     dispatch({ type: 'increment' });
+  };
+
+  const increaseHandler = () => {
+    dispatch({ type: 'increase', amount: 5 });
   };
 
   const decrementHandler = () => {
     dispatch({ type: 'decrement' });
   };
 
+  const decreaseHandler = () => {
+    dispatch({ type: 'decrease', amount: 5 });
+  };
+
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'toggle' });
+  };
+
   return (
     <main>
       <Card className={styles.counter}>
         <h1 className={styles['counter__title']}>REDUX COUNTER</h1>
-        <div className={styles['counter__value']}>{counter}</div>
+        {show && <div className={styles['counter__value']}>{counter}</div>}
         <div className={styles['counter__actions']}>
+          <Button
+            className={styles['counter__button']}
+            onClick={decreaseHandler}
+          >
+            -5
+          </Button>
           <Button
             className={styles['counter__button']}
             onClick={decrementHandler}
           >
             -
           </Button>
+
           <Button
             className={styles['counter__button']}
             onClick={incrementHandler}
           >
             +
           </Button>
+          <Button
+            className={styles['counter__button']}
+            onClick={increaseHandler}
+          >
+            +5
+          </Button>
         </div>
-        <Button className={styles['counter__button']}>Toggle Counter</Button>
+        <Button
+          className={styles['counter__button']}
+          onClick={toggleCounterHandler}
+        >
+          Toggle Counter
+        </Button>
       </Card>
     </main>
   );
