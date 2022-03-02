bb9b5f61bfb58898c69342fac4e310c9b5bf00a0
Zoltán Völcsey <zvolcsey@gmail.com>
2022-03-02 14:25:25 +0100

    add counter functionality

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,13 +1,43 @@
+import { useSelector, useDispatch } from 'react-redux';
+
 import styles from './Counter.module.css';
 import Card from '../UI/Card';
+import Button from '../UI/Button';
 
 const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+
+  const incrementHandler = () => {
+    dispatch({ type: 'increment' });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: 'decrement' });
+  };
+
   return (
-    <Card className={styles.counter}>
-      <p className={styles['counter__title']}>REDUX COUNTER</p>
-      <p className={styles['counter__value']}>-- COUNTER VALUE --</p>
-      <button>Toggle Counter</button>
-    </Card>
+    <main>
+      <Card className={styles.counter}>
+        <h1 className={styles['counter__title']}>REDUX COUNTER</h1>
+        <div className={styles['counter__value']}>{counter}</div>
+        <div className={styles['counter__actions']}>
+          <Button
+            className={styles['counter__button']}
+            onClick={decrementHandler}
+          >
+            -
+          </Button>
+          <Button
+            className={styles['counter__button']}
+            onClick={incrementHandler}
+          >
+            +
+          </Button>
+        </div>
+        <Button className={styles['counter__button']}>Toggle Counter</Button>
+      </Card>
+    </main>
   );
 };
 
